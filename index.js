b65f61d28fadf0666862f1c9e733fa8992c79a39
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-05 14:20:26 -0700

    commit tree 4a8bcf3b7a78ae432b247480c401f111b8ff1ea5

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This one is on me!";
+  } else if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  const message = (cityName === 'NYC') ? "Ok, sounds good." : "No go.";
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch (tip){
+    case 'thanks for everything':
+      message = "Bye.";
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+    case 'generous':
+      message = "Thank you so much.";
+      break
+  }
+  return message;
 }
